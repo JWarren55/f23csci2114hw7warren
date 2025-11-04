78f39fa5f765796bbe5374f244a771e6dbdf058c
JWarren55 <jonwwarren55@gmail.com>
2025-11-04 11:06:04 -0600

    Solved warren03

---
+++ warren3.java
@@ -0,0 +1,45 @@
+public class warren3 {
+    public static void main(String[] args) {
+        long target = 600851475143L;
+        int[] primes = Eratosthenes(Math.sqrt(target));
+        int len = primes.length -1;
+
+        int largestPrimeFactor = 0;
+        
+        for(int i = len; i > 1; i--) {
+            if(primes[i] != 0) {
+                if(target % primes[i] == 0) {
+                    largestPrimeFactor = primes[i];
+                    // System.out.println(primes[i]);
+                }
+            }
+        }
+        System.out.println(largestPrimeFactor);
+    }
+
+    private static int[] Eratosthenes(double n) {
+        boolean[] places = new boolean[(int)n];
+        int m = 0;
+
+        for (int i = 2; i < Math.sqrt(places.length); i++) {
+            for (int j = i + i; j < places.length; j+= i) {
+                if(j%i == 0) {
+                    places[j] = true;
+                    m++;
+                }
+            }
+        }
+
+        int[] primes = new int[m];
+        int x = 0;
+
+        for (int i = places.length - 1; i > 0; i--) {
+            if (!places[i]) {
+                primes[x] = i;
+                x++;
+            }
+        }
+
+        return primes;
+    }
+}
