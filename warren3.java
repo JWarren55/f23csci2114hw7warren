1a003c39c51ad56ff60e542b27586bdd344b9014
JWarren55 <jonwwarren55@gmail.com>
2025-11-05 09:51:51 -0600

    Add Instructions
    Add instructions to 1, 2, and 3

--- warren3.java
+++ warren3.java
@@ -1,4 +1,7 @@
 public class warren3 {
+    //The prime factors o 13195 are 5, 7, 13 and 29.
+    //
+    //What is the largest prime factor of the number 600851475143?
     public static void main(String[] args) {
         long target = 600851475143L;
         int[] primes = Eratosthenes(Math.sqrt(target));
